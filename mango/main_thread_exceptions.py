acd67ced0ed0c364483940edf0e30a4f8b13f66b
Vít Šesták <vitek@chainkeepers.io>
2021-12-09 21:43:17 +0100

    [lib] Fix error when trying to exit

--- mango/main_thread_exceptions.py
+++ mango/main_thread_exceptions.py
@@ -11,7 +11,7 @@ def wrap_main(main):
         print("main thread failed")
         traceback.print_exc()
         # I would prefer calling sys.exit(42), but it doesn't exit until other threads do so
-        os.execl("/usr/bin/bash", 'bash', '-c', 'exit 42')
+        os.execl("/bin/bash", 'bash', '-c', 'exit 42')
 
 def wrap_async_main(main):
     wrap_main(lambda: asyncio.run(main()))
