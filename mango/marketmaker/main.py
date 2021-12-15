23bedec974a7a2d7792a240f22627391a6794c65
Ondra Chaloupka <chalda@chainkeepers.io>
2021-12-15 13:35:32 +0100

    [DELETE ME LATER] transfering main marketmaker from chkp/core

--- mango/marketmaker/main.py
+++ mango/marketmaker/main.py
@@ -15,7 +15,7 @@ import mango.marketmaking
 from mango.main_thread_exceptions import wrap_main
 from mango.marketmaking.modelvalues import ModelValuesGraph
 from mango.marketmaking.orderchain.chain import Chain
-from mango.marketmaking.orderchain.chainbuilder import get_simple_orderchain
+from mango.marketmaking.orderchain.chkpchains import get_simple_orderchain
 from mango.configuration import load_configuration
 from mango.orders import OrderType
 
@@ -28,7 +28,9 @@ def dumpstacks(signal, frame):
 
     id2name = dict([(th.ident, th.name) for th in threading.enumerate()])
     code = []
-    for threadId, stack in sys._current_frames().items():
+    # noinspection PyProtectedMember
+    frames = sys._current_frames()
+    for threadId, stack in frames.items():
 
         code.append("\n# Thread: %s(%d)" % (id2name.get(threadId, ""), threadId))
         for filename, lineno, name, line in traceback.extract_stack(stack):
@@ -48,25 +50,36 @@ def parse_args(args=None):
     mango.ContextBuilder.add_command_line_parameters(parser)
     mango.Wallet.add_command_line_parameters(parser)
     parser.add_argument(
-        "-n", "--dry-run",
-        action="store_true", default=False,
-        help="runs as read-only and does not perform any transactions"
+        "--notify-errors",
+        type=mango.parse_notification_target,
+        action="append",
+        default=[],
+        help="The notification target for error events",
+    )
+    parser.add_argument(
+        "-n",
+        "--dry-run",
+        action="store_true",
+        default=False,
+        help="runs as read-only and does not perform any transactions",
     )
 
     return parser.parse_args()
 
 
 def cleanup(
-        context: mango.Context,
-        wallet: mango.Wallet,
-        account: mango.Account,
-        market: mango.Market,
-        dry_run: bool
+    context: mango.Context,
+    wallet: mango.Wallet,
+    account: mango.Account,
+    market: mango.Market,
+    dry_run: bool,
 ):
     market_operations: mango.MarketOperations = mango.create_market_operations(
-        context, wallet, account, market, dry_run)
-    builder: mango.MarketInstructionBuilder = \
-        mango.create_market_instruction_builder(context, wallet, account, market, dry_run)
+        context, wallet, account, market, dry_run
+    )
+    builder: mango.MarketInstructionBuilder = mango.create_market_instruction_builder(
+        context, wallet, account, market, dry_run
+    )
     cancels: mango.CombinableInstructions = mango.CombinableInstructions.empty()
     orders = market_operations.load_my_orders()
     for order in orders:
@@ -82,7 +95,6 @@ def cleanup(
 
 def override_args(cfg, args):
     args.cluster_url = [cfg.account.cluster_url]
-    args.account_index = 0  # index of the account to use, if more than one available
     args.confidence_interval_level = cfg.marketmaker.confidence_interval_level
     args.position_size_ratios = cfg.marketmaker.position_size_ratios
     args.order_type = OrderType.POST_ONLY
@@ -100,11 +112,10 @@ def setup_logging(args):
     logging.getLogger().setLevel(logging.INFO)
     logging.warning(mango.WARNING_DISCLAIMER_TEXT)
 
-    # CHKP TODO
-    # for notify in args.notify_errors:
-    #    handler = mango.NotificationHandler(notify)
-    #    handler.setLevel(logging.ERROR)
-    #    logging.getLogger().addHandler(handler)
+    for notify in args.notify_errors:
+        handler = mango.NotificationHandler(notify)
+        handler.setLevel(logging.ERROR)
+        logging.getLogger().addHandler(handler)
 
 
 def main():
@@ -120,6 +131,7 @@ def main():
     def handle_exit(signal, frame):
         dumpstacks(signal, frame)
         exit_waiter.set()
+
     signal(SIGTERM, handle_exit)
     signal(SIGQUIT, handle_exit)
     signal(SIGINT, handle_exit)
@@ -127,20 +139,18 @@ def main():
     def thread_exception_handler(args, /):
         traceback.print_exc()
         exit_waiter.set()  # When a thread fails, make the main thread to exit
+
     threading.excepthook = thread_exception_handler
 
     context = mango.ContextBuilder.from_command_line_parameters(args)
     logging.info(f"{context}")
-    wallet = mango.Wallet.from_command_line_parameters(args) \
-        or mango.Wallet.load(cfg.account.key_file)
+
+    wallet = mango.Wallet.from_command_line_parameters(args) or mango.Wallet.load(
+        cfg.account.key_file
+    )
     group = mango.Group.load(context, context.group_address)
     try:
-        account = mango.Account.load_for_owner_by_address(
-            context,
-            wallet.address,
-            group,
-            None
-        )
+        account = mango.Account.load_for_owner_by_address(context, wallet.address, group, None)
     except Exception as e:
         logging.error(f'Could not find any Mango accounts for owner {wallet.address}: {e}')
         account = None
@@ -161,7 +171,7 @@ def main():
         if market.quote != group.shared_quote_token:
             raise Exception(
                 f"Group {group.name} uses shared quote token"
-                f" {group.shared_quote_token.token.symbol}/{group.shared_quote_token.mint},"
+                f" {group.shared_quote_token.symbol}/{group.shared_quote_token.mint},"
                 f" but market {market.symbol} uses"
                 f" quote token {market.quote.symbol}/{market.quote.mint}."
             )
@@ -175,20 +185,16 @@ def main():
             Decimal(cfg.marketmaker.existing_order_quantity_tolerance),
         )
 
-        market_operations: mango.MarketOperations = \
-            mango.create_market_operations(
-                context,
-                wallet,
-                account,
-                market,
-                args.dry_run
-            )
+        market_operations: mango.MarketOperations = mango.create_market_operations(
+            context, wallet, account, market, args.dry_run
+        )
 
         is_perp = isinstance(market, mango.PerpMarket)
         desired_orders_chain: Chain = get_simple_orderchain(cfg.marketmaker, is_perp)
         model_values_graph = ModelValuesGraph(cfg.marketmaker, is_perp)
-        market_instruction_builder: mango.MarketInstructionBuilder = \
+        market_instruction_builder: mango.MarketInstructionBuilder = (
             mango.create_market_instruction_builder(context, wallet, account, market, args.dry_run)
+        )
         market_maker = mango.marketmaking.MarketMaker(
             wallet,
             market,
@@ -197,10 +203,10 @@ def main():
             desired_orders_chain,
             model_values_graph,
             order_reconciler,
-            args.redeem_threshold
+            args.redeem_threshold,
         )
 
-        model_state_builder: mango.marketmaking.ModelStateBuilder = \
+        model_state_builder: mango.marketmaking.ModelStateBuilder = (
             mango.marketmaking.model_state_builder_factory(
                 cfg,
                 args.update_mode,
@@ -212,30 +218,32 @@ def main():
                 group,
                 account,
                 market,
-                cfg.marketmaker.oracle_providers
+                cfg.marketmaker.oracle_providers,
             )
+        )
 
         health_check.add("marketmaker_pulse", market_maker.pulse_complete)
 
         if account is not None:
             logging.info(f"Current assets in account {account.address} (owner: {account.owner}):")
             net_values = [net_value for net_value in account.net_values if net_value is not None]
-            mango.InstrumentValue.report(net_values)
+            mango.InstrumentValue.report(net_values, logging.info)
 
         manager.open()
 
         def on_next(_):
-            return market_maker.pulse(
-                context,
-                model_state_builder.build(context)
-            )
+            return market_maker.pulse(context, model_state_builder.build(context))
 
-        pulse_disposable = rx.interval(cfg.marketmaker.poll_interval_seconds).pipe(
-            rxop.observe_on(context.create_thread_pool_scheduler()),
-            rxop.start_with(-1),
-            rxop.catch(mango.observable_pipeline_error_reporter),
-            rxop.retry()
-        ).subscribe(on_next)
+        pulse_disposable = (
+            rx.interval(cfg.marketmaker.poll_interval_seconds)
+            .pipe(
+                rxop.observe_on(context.create_thread_pool_scheduler()),
+                rxop.start_with(-1),
+                rxop.catch(mango.observable_pipeline_error_reporter),
+                rxop.retry(),
+            )
+            .subscribe(on_next)
+        )
 
         disposer.add_disposable(pulse_disposable)
 
