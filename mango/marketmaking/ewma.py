d84c9ef8dfcd46a3732d913a9d111fe49dcf3fa0
Marek Hauzr <marek.hauzr@gmail.cz>
2021-12-20 13:46:29 +0100

    [marketmaking/ewma] - addition of optional timestamp in update method

--- mango/marketmaking/ewma.py
+++ mango/marketmaking/ewma.py
@@ -1,4 +1,5 @@
 from typing import Optional
+from datetime import datetime
 from decimal import Decimal
 import math
 import time
@@ -19,15 +20,19 @@ class EWMA:
 
         self._latest_timestamp: Optional[float] = None
 
-    def update(self, value: Decimal) -> None:
+    def update(self, value: Decimal, now: Optional[datetime] = None) -> None:
+        if now is None:
+            _newest_timestamp = time.time()
+        else:
+            _newest_timestamp = now.timestamp()
+
         if self.latest is None:
             self.latest = value
-            self._latest_timestamp = time.time()
+            self._latest_timestamp = _newest_timestamp
         else:
             if self.halflife == Decimal('0'):
                 self.latest = value
             else:
-                _newest_timestamp = time.time()
                 t_delta = _newest_timestamp - self._latest_timestamp
                 alpha = Decimal(1 - math.exp(- math.log(2) / float(self.halflife) * t_delta))
                 self.latest = alpha * value + (1 - alpha) * self.latest
